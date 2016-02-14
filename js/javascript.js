715d2822ac592b2792f79fd7603feb815934f9e9
jaredgeizer <jaredg@Jareds-MacBook-Pro.local>
2016-02-13 22:53:51 -0500

    this is the first repository
    i have laid out alot of the structure, the landing page is done except
    for the mobile site. the work page is in progress, and i haven’t mad
    the contact page yet

---
+++ js/javascript.js
@@ -0,0 +1,4 @@
+//if then statement once window gets under 1600px
+//set the margin-left of the objects lvl3.3 and lvl4
+//set it by taking the window width and dividing it by a proportion then making the number negative
+//apply number to margin-left
